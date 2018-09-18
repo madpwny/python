d7af52dc5eba47c529ada8d61d4ff0730f63fb22
madpwny <madpwny@gmail.com>
2018-09-18 15:35:01 -0700

    finished lesson 8, functions

---
+++ lesson8/func1.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+import exp
+
+base = input('Please enter the base value:')
+exponent = input('Please enter the exponent value:')
+
+exp.exp(base,exponent)
+
---
+++ lesson8/func2.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import def1
+
+x = "This is the starting value."
+
+base = input('Please enter the base value:')
+exponent = input('Please enter the exponent value:')
+
+def1.exp(base,exponent)
+
+def1.printFib(23)
+
+fibSeries = def1.calcFib(23)
+print(fibSeries)
+
+def1.byValExample(x)
+
+print(x)
