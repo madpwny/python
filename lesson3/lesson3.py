b97d6a00d044c74fb918d08b2ac092b5d07758be
madpwny <madpwny@gmail.com>
2018-09-18 10:38:30 -0700

    starting lesson 4

---
+++ lesson3/lesson3.py
@@ -0,0 +1,12 @@
+UserName = 'madpwny'
+FullName = 'Stefan Loeve'
+
+print(UserName)
+print(FullName)
+
+AltName = UserName
+IncorrectName = 'FullName'
+NewName = UserName = FullName
+NewName = "New Name" = FullName
+print(NewName)
+print(UserName)
